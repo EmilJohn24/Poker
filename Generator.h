561198fae5e8f4523e72e3eef4a74cf05feb1357
Emil John Lopez <emiljohn24@gmail.com>
2019-06-27 22:45:57 +0800

    Added essential abstract functions for generator

--- Generator.h
+++ Generator.h
@@ -1,8 +1,16 @@
 #ifndef GENERATOR_H
 #define GENERATOR_H
+#include <vector>
 class Generator {
 	//abstract card generation class
-	
+	public:
+		typedef vector<Card> Pile;
+		Generator() = default;
+		virtual Card& generateCard() = 0; //method for spitting out new cards
+		Pile generatePile(int amount) = 0;
+		virtual bool isDone() = 0;
+		virtual void update() = 0; //method for updating internal components
+
 }
 
 #endif
